cf3ea9e1c86388d5f036e9fd0a85428dbbc95c0d
Disruptive Digital <66871992+disruptivedigital@users.noreply.github.com>
2021-03-31 16:28:32 +0300

    v4.2
    - script update: sudo chown -R $USER $HOME

--- netdata-install-config.sh
+++ netdata-install-config.sh
@@ -5,7 +5,7 @@
 
 # Starting...
 printf "Updating Linux..."
-sudo chown -R $USER /home
+sudo chown -R $USER $HOME
 sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
 sudo apt -y autoremove && sudo apt-get -y autoclean
 
